fd0e71fcd8354f7b9b1a7e906c0123203b5079b4
Zachary Parker <EvergreenZach@gmail.com>
2024-06-03 15:23:23 -0600

    added table for products

--- SoloProjectReact/client/src/views/Home.jsx
+++ SoloProjectReact/client/src/views/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import { Table } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+            <h2>Products for Sale</h2>
+          <Table striped bordered hover variant='info' className='mx-auto my-auto border border-0 rounded-2 py-2' >
+              <thead className='border border-0 rounded-2'>
+                  <tr>
+                      <th>Name</th>
+                      <th>Date Posted</th>
+                      <th>Price</th>
+                      <th>Owner</th>
+                  </tr>
+              </thead>
+              <tbody>
+
+
+
+              </tbody>
+          </Table>
+    </Container>
   )
 }
 
